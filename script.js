7872168ccbae3cf07dc602993ab5f10f8a1ed69e
raziru <63628772+raziru@users.noreply.github.com>
2021-10-06 11:20:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ async function start() {
   // Load the TFLite model.
   const model = await tfTask.NLClassification.CustomModel.TFLite.load({
     model:
-      "https://storage.googleapis.com/tfweb/models/movie_review_sentiment_classification.tflite"
+    "https://storage.googleapis.com/tfweb/models/movie_review_sentiment_classification.tflite"
   });
 
   document.querySelector(".btn").addEventListener("click", async () => {
