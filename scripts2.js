68602349481810894a4179de767c024bfcc0a26f
raziru <63628772+raziru@users.noreply.github.com>
2021-10-08 17:24:12 +0900

    Update scripts2.js

--- scripts2.js
+++ scripts2.js
@@ -1,1 +1,28 @@
+async function start() {
+  const textarea = document.querySelector("textarea");
+  const resultDiv = document.querySelector(".result");
 
+  // Load the TFLite model.
+  
+  const model = await tfTask.NLClassification.CustomModel.TFLite.load({
+    model:
+    "https://storage.googleapis.com/tfweb/models/movie_review_sentiment_classification.tflite"
+    //"./movie_review_sentiment_classification.tflite"
+    //"./mymodelLSTM.tflite"
+  });
+  
+  //const model = await tf.loadLayersModel('./tensorflowjs-model/model.json');
+  
+  document.querySelector(".btn").addEventListener("click", async () => {
+    // Get the classification result for the entered text
+    var temp=textarea.value;
+    const result = await model.predict(temp);
+
+    // Show the results.
+    resultDiv.textContent = result.classes
+      .map((c) => `${c.className}: ${c.score.toFixed(3)}`)
+      .join(", ");
+  });
+}
+
+start();
