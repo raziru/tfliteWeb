613852af695bc508dcdf52886640ec1e4a1531ed
raziru <63628772+raziru@users.noreply.github.com>
2021-10-08 16:36:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ async function start() {
   const resultDiv = document.querySelector(".result");
 
   // Load the TFLite model.
-  
+  /*
   const model = await tfTask.NLClassification.CustomModel.TFLite.load({
     model:
     "https://storage.googleapis.com/tfweb/models/movie_review_sentiment_classification.tflite"
     //"./movie_review_sentiment_classification.tflite"
     //"./mymodelLSTM.tflite"
-  });
+  });*/
   
-  //const model = await tf.loadLayersModel('./movie_review_sentiment_classification.tflite');
-
+  const model = await tf.loadLayersModel('./tensorflowjs-model/model.json');
+  
   document.querySelector(".btn").addEventListener("click", async () => {
     // Get the classification result for the entered text
     var temp=textarea.value;
