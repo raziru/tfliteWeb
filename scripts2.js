ef0f5536fd38e19ae24f3ee735346c1db5211f1a
raziru <63628772+raziru@users.noreply.github.com>
2021-10-08 19:07:46 +0900

    Update scripts2.js

--- scripts2.js
+++ scripts2.js
@@ -2,14 +2,27 @@ const MAX_SEQUENCE_LENGTH = 113;
 const getKey = (obj,val) => Object.keys(obj).find(key => obj[key] === val); // For getting tags by tagid
 
 
-let model, emodel;
-(async function() {
-    model = await tf.loadLayersModel('http://localhost:8081/tfjs_models/ner/model.json');
-    let outputs_ = [model.output, model.getLayer("attention_vector").output];
-    emodel = tf.model({inputs: model.input, outputs: outputs_});
-    $('.loading-model').remove();
-})();
+async function start() {
+  const textarea = document.querySelector("textarea");
+  const resultDiv = document.querySelector(".result");
+  // Load the TFLite model.
+  const model = await tfTask.NLClassification.CustomModel.TFLite.load({
+    model:
+    "https://storage.googleapis.com/tfweb/models/movie_review_sentiment_classification.tflite"
+    //"https://storage.googleapis.com/tensorflow_mine/mymodelLSTM.tflite"
+  });
 
+  document.querySelector(".btn").addEventListener("click", async () => {
+    // Get the classification result for the entered text
+    var temp=textarea.value;
+    const result = await model.predict(temp);
+    // Show the results.
+    resultDiv.textContent = result.classes
+      .map((c) => `${c.className}: ${c.score.toFixed(3)}`)
+      .join(", ");
+  });
+}
+start();
 
 function word_preprocessor(word) {
   word = word.replace(/[-|.|,|\?|\!]+/g, '');
