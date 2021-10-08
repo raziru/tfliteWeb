d40a4f9572eeab4852be484efa478c60c1b8e483
raziru <63628772+raziru@users.noreply.github.com>
2021-10-08 17:49:00 +0900

    Update scripts2.js

--- scripts2.js
+++ scripts2.js
@@ -1,28 +1,46 @@
-async function start() {
-  const textarea = document.querySelector("textarea");
-  const resultDiv = document.querySelector(".result");
+const MAX_SEQUENCE_LENGTH = 113;
+const getKey = (obj,val) => Object.keys(obj).find(key => obj[key] === val); // For getting tags by tagid
 
-  // Load the TFLite model.
-  
-  const model = await tfTask.NLClassification.CustomModel.TFLite.load({
-    model:
-    "https://storage.googleapis.com/tfweb/models/movie_review_sentiment_classification.tflite"
-    //"./movie_review_sentiment_classification.tflite"
-    //"./mymodelLSTM.tflite"
+
+let model, emodel;
+(async function() {
+    model = await tf.loadLayersModel('http://localhost:8081/tfjs_models/ner/model.json');
+    let outputs_ = [model.output, model.getLayer("attention_vector").output];
+    emodel = tf.model({inputs: model.input, outputs: outputs_});
+    $('.loading-model').remove();
+})();
+
+
+function word_preprocessor(word) {
+  word = word.replace(/[-|.|,|\?|\!]+/g, '');
+  word = word.replace(/\d+/g, '1');
+  word = word.toLowerCase();
+  if (word != '') {
+    return word;
+  } else {
+    return '.'
+  }
+};
+
+function make_sequences(words_array) {
+  let sequence = Array();
+  words_array.slice(0, MAX_SEQUENCE_LENGTH).forEach(function(word) {
+    word = word_preprocessor(word);
+    let id = words_vocab[word];
+    if (id == undefined) {
+      sequence.push(words_vocab['<UNK>']);
+    } else {
+      sequence.push(id);
+    }  
   });
-  
-  //const model = await tf.loadLayersModel('./tensorflowjs-model/model.json');
-  
-  document.querySelector(".btn").addEventListener("click", async () => {
-    // Get the classification result for the entered text
-    var temp=textarea.value;
-    const result = await model.predict(temp);
 
-    // Show the results.
-    resultDiv.textContent = result.classes
-      .map((c) => `${c.className}: ${c.score.toFixed(3)}`)
-      .join(", ");
-  });
-}
+  // pad sequence
+  if (sequence.length < MAX_SEQUENCE_LENGTH) {
+    let pad_array = Array(MAX_SEQUENCE_LENGTH - sequence.length);
+    pad_array.fill(words_vocab['<UNK>']);
+    sequence = sequence.concat(pad_array);
+  }
 
-start();
+  return sequence;
+};
+
